8de8d776eb050934ad78ef082039f072a64f04b2
Pawel <wispawelwis38@gmail.com>
2020-03-30 19:19:34 +0200

    added csp, improved victory check

---
+++ mainpackage/csp.py
@@ -0,0 +1,5 @@
+class CSP:
+    def __init__(self, variables, domains, constrains):
+        self.variables = variables
+        self.domains = domains
+        self.constrains = constrains
--- mainpackage/sudoku.py
+++ mainpackage/sudoku.py
@@ -22,33 +22,57 @@ class Sudoku:
             self.puzzle = arr_to_square(list(line[2]))
             self.solution = arr_to_square(list(line[3])) if len(line) == 4 else None
             self.size = int(len(list(line[2])) ** 0.5)
+            self.variables = []
+            self.setup_variables()
+            self.domains = []
+            self.setup_domains()
+
+    def setup_variables(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.puzzle[i][j] == '.':
+                    self.variables.append((i, j))
+
+    def setup_domains(self):
+        for i in range(len(self.variables)):
+            self.domains.append([x for x in range(1, self.size + 1)])
 
     def get_box_left_top_corner(self, i, j):  # > v (row, column)
         a = int(self.size ** 0.5)
         return int(i // a * a), int(j // a * a)
 
-    def check_victory(self):
+    def constrains(self):
         for row in self.puzzle:  # rows
-            if len(set(row)) != self.size:
-                return False
+            numbers_in_row = []
+            for number in row:
+                if number != '.':
+                    if number in numbers_in_row:
+                        return False
+                    else:
+                        numbers_in_row.append(number)
 
         for i in range(self.size):  # columns
-            number_set = set()
+            numbers_in_col = []
             for j in range(self.size):
-                number_set.add(self.puzzle[j][i])
-            if len(number_set) != self.size:
-                return False
+                number = self.puzzle[j][i]
+                if number != '.':
+                    if number in numbers_in_col:
+                        return False
+                    else:
+                        numbers_in_col.append(number)
 
         box_size = int(self.size ** 0.5)  # boxes
         for i in range(box_size):
             for j in range(box_size):
-                number_set = set()
+                numbers_in_box = []
                 for row in range(box_size):
-                    row_numbers = self.puzzle[i * box_size + row][j * box_size:(j + 1) * box_size]
-                    for number in row_numbers:
-                        number_set.add(number)
-                if len(number_set) != self.size:
-                    return False
+                    subrow_numbers = self.puzzle[i * box_size + row][j * box_size:(j + 1) * box_size]
+                    for number in subrow_numbers:
+                        if number != '.':
+                            if number in numbers_in_box:
+                                return False
+                            else:
+                                numbers_in_box.append(number)
 
         return True
 
@@ -64,65 +88,7 @@ class Sudoku:
             print()
 
 
-
-
-
-# s.print_solution()
-
-
-def brute_force_sudoku(sudoku):
-    constants = []
-    for i in range(sudoku.size):
-        for j in range(sudoku.size):
-            if sudoku.puzzle[i][j] != '.':
-                constants.append((i, j))
-    print(constants)
-
-    def get_next(i, j):
-        j = j - 1
-        if j < 0:
-            j = sudoku.size - 1
-            i -= 1
-        return i, j
-
-    def increment(i, j):
-        if (i, j) in constants:
-            # sudoku.print_puzzle()
-            i, j = get_next(i, j)
-            if i == -1:
-                return False
-            return increment(i, j)
-        current = int(sudoku.puzzle[i][j])
-        current += 1
-        if current > sudoku.size:
-            sudoku.puzzle[i][j] = '1'
-            i, j = get_next(i, j)
-            if i==-1:
-                return False
-            return increment(i,j)
-        else:
-            sudoku.puzzle[i][j] = str(current)
-            if sudoku.check_victory():
-                print(sudoku.print_puzzle())
-                raise ValueError("SUCCESS")
-        return True
-
-    for i in range(sudoku.size):
-        for j in range(sudoku.size):
-            if sudoku.puzzle[i][j] =='.':
-                sudoku.puzzle[i][j] = '1'
-
-    sudoku.print_puzzle()
-    i=0
-    while increment(sudoku.size-1,sudoku.size-1) == False:
-        i+=1
-        if i%1000000==0:
-            sudoku.print_puzzle()
-
-
-
 # print(s.get_box_left_top_corner(2, 5))
 s = Sudoku(1)
 s.print_solution()
 s.print_puzzle()
-brute_force_sudoku(s)
